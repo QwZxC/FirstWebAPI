531e81237bd93a1f59dcbbfb96bf64ff1b4396e2
denischagin <denischaginnn@gmail.com>
2023-05-22 06:26:35 +0300

    create Alert Component

--- frontend/src/components/Alert/Alert.tsx
+++ frontend/src/components/Alert/Alert.tsx
@@ -2,11 +2,13 @@ import React, { FC } from 'react'
 
 interface AlertProps {
 	children: any,
+  open: boolean,
+  setOpen: (open: boolean) => void
 
 }
 
 export const Alert: FC<AlertProps>= ({children}) => {
   return (
-    <div className=''>{}</div>
+    <div className=''>{children}</div>
   )
 }
