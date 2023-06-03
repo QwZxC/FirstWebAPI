77f3745d1a0a383d2f10ee2e1b105fb45ab1098e
denischagin <denischaginnn@gmail.com>
2023-06-03 16:19:39 +0300

    create delete theme request

--- frontend/src/components/ThemesList/ThemesItem/ThemesItem.tsx
+++ frontend/src/components/ThemesList/ThemesItem/ThemesItem.tsx
@@ -1,16 +1,42 @@
-import { FC } from "react"
-import { ITheme } from "../../../models/ITheme"
-import { ListItem, Typography } from '@mui/material';
+import { FC } from 'react'
+import { ITheme } from '../../../models/ITheme'
+import { IconButton, ListItem, Typography } from '@mui/material'
+import { Remove } from '@mui/icons-material'
+import { useMutation, useQueryClient } from 'react-query';
+import { deleteTheme } from './../../../services/themes';
 
 interface ThemesItemProps {
-	theme: ITheme
+  theme: ITheme
 }
 
 export const ThemesItem: FC<ThemesItemProps> = ({ theme }) => {
-	const { name } = theme
+  const { name, id } = theme
+
+	const client = useQueryClient()
+
+	const {mutate: deleteThemeById} = useMutation({
+		mutationFn: deleteTheme,
+		onSuccess: () => {
+			client.invalidateQueries({
+				queryKey: ['lessons']
+			})
+		}		
+	})
+
+	const deleteHandler = () => {
+		if (id === undefined) return
+		deleteThemeById(id)
+	}
+
   return (
     <ListItem>
-			<Typography variant="h4" component="p">{name}</Typography>
-		</ListItem>
+      <Typography variant='h4' component='p' sx={{flexGrow: 1}}>
+        {name}
+      </Typography>
+
+			<IconButton onClick={deleteHandler}>
+				<Remove color='error'/>
+			</IconButton>
+    </ListItem>
   )
 }
