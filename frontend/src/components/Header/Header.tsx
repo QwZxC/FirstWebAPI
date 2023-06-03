99f8618d83ad953f38c027b16d44da10135ef3a7
denischagin <denischaginnn@gmail.com>
2023-06-03 17:11:09 +0300

    add site header name

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,21 +1,32 @@
-import { AppBar, Button, Toolbar } from '@mui/material'
+import { AppBar, Box, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import { lessonUrl } from '../../constants/routes'
+import { lessonUrl, startPage } from '../../constants/routes'
 
 export const Header = () => {
   const navigate = useNavigate()
   return (
     <AppBar position='static'>
       <Toolbar>
-        <Button
-          sx={{color: "white"}}
-          onClick={() => {
-            navigate(lessonUrl, { replace: true })
-          }}
-        >
-          Занятия
-        </Button>
+        <Box display='flex' gap='10px' alignItems='center' width='100%'>
+          <Typography
+            variant='h4'
+            sx={{ flexGrow: 1, cursor: 'pointer' }}
+            onClick={() => navigate(startPage, { replace: true })}
+          >
+            WebJournal
+          </Typography>
+
+          <Typography
+            variant='h5'
+            sx={{ color: 'white', cursor: 'pointer' }}
+            onClick={() => {
+              navigate(lessonUrl, { replace: true })
+            }}
+          >
+            Занятия
+          </Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   )
